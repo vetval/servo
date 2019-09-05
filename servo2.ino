3d7e30b0b031d6a7db553fe5c8562f9a5ea33b65
vetval <49076196+vetval@users.noreply.github.com>
2019-09-05 17:27:08 +0300

    Add files via upload
    rev1

--- servo2.ino
+++ servo2.ino
@@ -3,10 +3,11 @@
 */
 
 #include <Servo.h>
-#define OPEN1_POS 180
-#define OPEN2_POS 180
-#define CLOSE1_POS 0
-#define CLOSE2_POS 0
+#define OPEN1_POS 150
+#define OPEN2_POS 150
+#define CLOSE1_POS 30
+#define CLOSE2_POS 30
+#define LED 13
 
 Servo servo1;  // create servo object to control a servo
 Servo servo2;  // create servo object to control a servo
@@ -16,19 +17,26 @@ int pos = 0;    // variable to store the servo position
 void setup() {
   servo1.attach(8);  // attaches the servo on pin 8 to the servo object
   servo2.attach(9);  // attaches the servo on pin 9 to the servo object
-  servo1.write(CLOSE1_POS);  // tell servo to go to close position
-  servo2.write(CLOSE2_POS);  // tell servo to go to close position
-  delay(500);       // waits 300ms for the servos to reach the position
-  servo1.write(OPEN1_POS);  // tell servo to go to open position
-  delay(2000);      
-  servo2.write(OPEN2_POS);  // tell servo to go to open position
-  delay(2000);      
-  servo2.write(CLOSE2_POS); 
-  delay(2000);              
-  servo2.write(CLOSE1_POS); 
-  delay(1000);              
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
 }
 
 void loop() {
+  servo1.write(CLOSE1_POS);  // tell servo to go to close position
+  servo2.write(CLOSE2_POS);  // tell servo to go to close position
+  delay(500);       // waits 300ms for the servos to reach the position
+  digitalWrite(LED, LOW);
+  servo1.write(OPEN1_POS);  // tell servo to go to open position
+  delay(2000);      
+  digitalWrite(LED, HIGH);
+  servo2.write(OPEN2_POS);  // tell servo to go to open position
+  delay(2000);      
+  digitalWrite(LED, LOW);
+  servo2.write(CLOSE2_POS); 
+  delay(2000);              
+  digitalWrite(LED, HIGH);
+  servo1.write(CLOSE1_POS); 
   delay(2000);                    
+  digitalWrite(LED, LOW);
+  while(1);
 }
